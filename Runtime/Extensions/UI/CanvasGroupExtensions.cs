b7546f2f37863001e0be280e59e34e5cde629485
Alex K <aiden.hapless@gmail.com>
2021-11-27 17:33:50 -0700

    Add CanvasGroup tweens

---
+++ Runtime/Extensions/UI/CanvasGroupExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace cmpy.Tween
+{
+    public static class CanvasGroupExtensions
+    {
+        public static FloatTween TAlpha(this CanvasGroup canvasGroup, float endValue, float duration)
+        {
+            FloatTween tween = new FloatTween(
+                canvasGroup,
+                () => canvasGroup.alpha,
+                (value) => canvasGroup.alpha = value,
+                endValue, duration);
+            TweenEngine.Instance.AddTween(tween);
+            return tween;
+        }
+    }
+}
